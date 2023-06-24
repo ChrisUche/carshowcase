5940cff6821782ba03bc2b508be47424a19362b7
Uche Chris <chrisuche58@yahoo.com>
2023-06-24 22:18:00 +0100

    button and button props

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+// consider converting part of this to a "Client Component"
+"use client"
+import React from 'react';
+import Image from 'next/image';
+import { CustomButtonProps } from '@/types';
 
-const CustomButton = () => {
+
+const CustomButton = ({title, containerStyles, handleClick}: CustomButtonProps) => {
   return (
-    <div>CustomButton</div>
+    <button 
+        disabled={false}
+        type={"button"}
+        className= {`custom-btn`}
+        onClick={() => {}}
+    >
+        <span className={`flex-1`}> title</span>
+    </button>
   )
 }
 
