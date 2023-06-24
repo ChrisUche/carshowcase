f9afcf225da5c575d9a6e8c13b2aad9a0c34d487
Uche Chris <chrisuche58@yahoo.com>
2023-06-24 22:36:30 +0100

    pictures

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,6 +20,11 @@ const handleScroll =() => {
                 handleClick={handleScroll}
             />
         </div>
+        <div className='hero__image-container'>
+            <div className='hero__image'>
+                <Image src="/hero.png" alt='hero' fill className='object-contain'/>
+            </div>
+        </div>
     </div>
   )
 }
