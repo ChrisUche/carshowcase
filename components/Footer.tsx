9d8986b24746788f98849dfae68fa23b115327ae
Uche Chris <chrisuche58@yahoo.com>
2023-06-25 18:49:55 +0100

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -29,6 +29,14 @@ const Footer = () => {
            ))}
         </div>
       </div>
+
+        <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 px-6 py-10 sm:px-16'>
+          <p>@2023 CarHub. All Rights Reserved</p>
+          <div className='footer__copyrights-link'>
+            <Link href={"/"} className='text-gray-500'> Privacy Policy</Link>
+            <Link href={"/"} className='text-gray-500'> Terms of Use</Link>
+          </div>
+        </div>
     </footer>
   )
 }
