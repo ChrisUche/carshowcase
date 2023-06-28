ef276bcdd9b4ff45dfd98739b5c5f75c45f4f7b1
Uche Chris <chrisuche58@yahoo.com>
2023-06-28 22:07:33 +0100

    fuel and year filter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+"use client"
 
-const CustomFilter = () => {
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { Fragment, useState } from 'react';
+import {Listbox, Transistion} from '@headlessui/react';
+import { CustomFilterProps } from '@/types';
+
+const CustomFilter = ({ title, options }: CustomFilterProps) => {
   return (
-    <div>CustomFilter</div>
+    <div className='w-fit'>
+      <Listbox>
+        <div className='relative w-fit z-10'>
+          <Listbox.Button className="custom-filter__btn">
+            <span>filter</span>
+          </Listbox.Button>
+        </div>
+      </Listbox>
+    </div>
   )
 }
 
